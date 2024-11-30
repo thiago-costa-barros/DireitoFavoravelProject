c88b31c59d29f7dbc8185e7db4887cf564e511a4
Seu Nome <seu@email.com>
2024-11-30 09:48:21 -0300

    fix: parseDate to returno timestamp

--- pages/api/utils/parseDate.ts
+++ pages/api/utils/parseDate.ts
@@ -1,4 +1,20 @@
-export function parseDate(bigintDate: bigint): Date {
-    return new Date(Number(bigintDate)); 
+export function parseDate(bigintDate: bigint): string {
+  const date = new Date(Number(bigintDate)); // Converte o bigint para um objeto Date
+  
+  // Garantir que a data é válida
+  if (isNaN(date.getTime())) {
+    throw new Error('Invalid date format');
+  }
+
+  // Formatar a data no formato YYYY-MM-DD HH:mm:ss.SSS
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const milliseconds = date.getMilliseconds().toString().padStart(3, '0');
+  
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
   }
   
